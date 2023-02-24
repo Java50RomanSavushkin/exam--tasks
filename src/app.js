a09b4e38f3a76e91fb6aa30a14b908220c870fef
RomanSavushkin <romarsa82@gmail.com>
2023-02-24 11:01:31 +0200

    test

---
+++ src/app.js
@@ -0,0 +1,8 @@
+"use strict";
+var _loop_1 = function (i) {
+    setTimeout(function () { return console.log(i); });
+};
+for (var i = 0; i < 3; i++) {
+    _loop_1(i);
+}
+//# sourceMappingURL=app.js.map
