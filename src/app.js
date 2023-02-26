e6b890c434f18a4a2ea59aaae5c2f881715f8834
RomanSavushkin <romarsa82@gmail.com>
2023-02-27 01:52:18 +0200

    HW-29

--- src/app.js
+++ src/app.js
@@ -1,8 +1,29 @@
 "use strict";
-var _loop_1 = function (i) {
-    setTimeout(function () { return console.log(i); });
-};
-for (var i = 0; i < 3; i++) {
-    _loop_1(i);
+const aChar = 'a'.charCodeAt(0);
+const zChar = 'z'.charCodeAt(0);
+function shiftRound(string, shift) {
+    shift < 0 ? shift = Math.abs(shift) : shift;
+    let checkedSymbol;
+    const strArr = [];
+    for (const char of string) {
+        checkedSymbol = checkSymbol(char);
+        if ((checkedSymbol + shift) > zChar) {
+            checkedSymbol = (aChar + Math.abs(zChar - (shift + checkedSymbol))) - 1;
+            strArr.push(String.fromCharCode(checkedSymbol));
+        }
+        else if (Number.isFinite(checkedSymbol)) {
+            let shiftedCode = checkedSymbol + shift;
+            strArr.push(String.fromCharCode(shiftedCode));
+        }
+        else {
+            strArr.push(char);
+        }
+    }
+    return strArr.join("");
 }
+function checkSymbol(symbol) {
+    let symbolCode = symbol.charCodeAt(0);
+    return (symbolCode >= aChar && symbolCode <= zChar) ? symbolCode : NaN;
+}
+console.log(shiftRound("b", -1));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,28 @@
-for (let i = 0; i < 3; i++) {
-    setTimeout(() => console.log(i))
+
+const aChar: number = 'a'.charCodeAt(0);
+const zChar: number = 'z'.charCodeAt(0);
+function shiftRound(string: string, shift: number): string {
+    shift < 0 ? shift = Math.abs(shift): shift;
+    let checkedSymbol: number;
+    const strArr: string[] = [];
+    for (const char of string) {
+        checkedSymbol = checkSymbol(char)
+        if ((checkedSymbol + shift) > zChar) {
+            checkedSymbol = (aChar + Math.abs(zChar - (shift + checkedSymbol))) - 1;
+            strArr.push(String.fromCharCode(checkedSymbol));
+        }
+        else if (Number.isFinite(checkedSymbol)) {
+            let shiftedCode = checkedSymbol + shift;
+            strArr.push(String.fromCharCode(shiftedCode));
+        }
+        else {
+            strArr.push(char)
+        }
+    }
+    return strArr.join("");
 }
+function checkSymbol(symbol: string): number {
+    let symbolCode = symbol.charCodeAt(0);
+    return (symbolCode >= aChar && symbolCode <= zChar) ? symbolCode : NaN;
+}
+console.log(shiftRound("b", -1));
