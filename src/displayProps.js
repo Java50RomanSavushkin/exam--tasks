71799758a81dc4edd6785b90f42793f3b5fc7c51
RomanSavushkin <romarsa82@gmail.com>
2023-02-28 21:25:36 +0200

    delete test

--- src/displayProps.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.displaySquarePerimeter = void 0;
-function displaySquarePerimeter(shape) {
-    console.log(`square of shape is ${shape.square()}
-perimeter of shape is ${shape.perimeter()}`);
-}
-exports.displaySquarePerimeter = displaySquarePerimeter;
-//# sourceMappingURL=displayProps.js.map
