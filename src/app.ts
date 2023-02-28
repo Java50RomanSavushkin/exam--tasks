c1d2c891e57642bae0c4c9d026ebec2ee3faa313
RomanSavushkin <romarsa82@gmail.com>
2023-02-28 20:40:49 +0200

    gitignore

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import { Container } from "./Container";
 const rectangle: Rectangle = new Rectangle(3, 4);
 const square: Square = new Square(10);
 const container: Container = new Container([rectangle, square])
-displaySquarePerimeter(container);
+displaySquarePerimeter(container)
 
 
 
