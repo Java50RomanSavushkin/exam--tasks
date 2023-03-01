2e192ea0bba8f69f1df787d2155862d8d4dc5fd2
RomanSavushkin <romarsa82@gmail.com>
2023-03-01 22:30:03 +0200

    hw-30

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,3 @@
-import { Rectangle } from "./Rectangle";
-import { Shape } from "./Shape";
-import { Square } from "./Square";
-import { displaySquarePerimeter } from "./displayProps";
-import { Container } from "./Container";
-const rectangle: Rectangle = new Rectangle(3, 4);
-const square: Square = new Square(10);
-const container: Container = new Container([rectangle, square])
-displaySquarePerimeter(container);
-
-
+//TODO
+//module for testing
 
